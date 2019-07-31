54c19f2b8374b088a567dbdf3028c848e9bd76a1
dealerbrsimon <53241567+dealerbrsimon@users.noreply.github.com>
2019-07-31 12:57:20 -0400

    Update main.py
    added comments to explain basic usage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 '''
-Exemple of usage with a main method. Instanciate a builder and a director that interacts to create new objects.
+Exemple d'utilisation par une méthode client(main) avec l'interface amusante pour illustrer le fonctionnement du programme
+aux développeurs.
 '''
 
 
@@ -9,6 +10,21 @@ from colorbuilder import colorbuilders as cb
 def main():
 
 	bob,marie = cb()
+	'''
+	L'invocation de cb retourne un constructeur ainsi qu'un directeur pour le constructeur en question.
+	Le directeur est déjà lié avec le constructeur via l'invocation de la méthode, ses invocations guident 
+	les actions du constructeur, qui aurait pu le faire de manière indépendante si on l'avais désiré.
+	Si vous désirer séparer les fonctionnalités du constructeur et du directeur, prière de me le faire 
+	savoir.
+	'''
+	
+	marie.fais_ca_vite
+	
+	bob.butch_la_job
+	
+	marie.fais_ca_bien
+
+	bob.prends_sontemp
 
 	
 
