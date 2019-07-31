8d02643db1615a7640e7fe3917ce04bcbfd6bf17
dealerbrsimon <53241567+dealerbrsimon@users.noreply.github.com>
2019-07-31 12:50:36 -0400

    Create main.py
    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Exemple of usage with a main method. Instanciate a builder and a director that interacts to create new objects.
+'''
+
+
+from colorbuilder import colorbuilders as cb 
+
+
+def main():
+
+	bob,marie = cb()
+
+	
+
+
+
+	return 0
+
+
+if __name__ == '__main__':
+	main()
